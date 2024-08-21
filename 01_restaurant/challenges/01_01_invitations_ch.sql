8d19c54e0509329f56071b6b712ab6317217b232
eversun4000t <evet950@icloud.com>
2024-08-21 22:40:29 +0000

    updates

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -2,3 +2,8 @@
 
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
+select firstname as [first name]
+, lastname as [last name]
+, email as [email adress]
+from Customers
+order by lastname;
